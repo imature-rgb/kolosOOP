24ca9b491d663a8d8e8c176a24a49d47cf3462d9
imature-rgb <imature95@gmail.com>
2024-11-20 23:55:16 +0100

    Update zadPierwsze.java

--- zadPierwsze.java
+++ zadPierwsze.java
@@ -1,3 +1,7 @@
+/*
+Używając metody charAt, napisz statyczną metodę, która sprawdza, czy dany napis zaczyna się i kończy tym samym znakiem. Stwórz przypadek testowy.
+*/
+
 public class FirstOrLastArgs{
     public static boolean gowno (String input){
         if(input == null || input.length()==0){
